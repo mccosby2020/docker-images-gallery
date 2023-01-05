f8aa4ee9369d627d3884e5b7e6366e5d64fb1cd9
Vamedekah3 <vamedekah3.gatech.edu>
2023-01-04 22:04:54 -0600

    initial project set up

---
+++ api/main.py
@@ -0,0 +1,41 @@
+"""main module to run the api service"""
+
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["DEBUG"] = DEBUG
+
+
+@app.route("/new-image")
+def new_image():
+    """Function to get new image from unsplash"""
+    word = request.args.get("query")
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
+    response = requests.get(
+        url=UNSPLASH_URL, headers=headers, params=params, timeout=10
+    )
+
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
